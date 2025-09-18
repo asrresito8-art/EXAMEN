c1e5a297f84c81409d6dded3d1604e74981a1483
SERCHXD <asrresito8@gmail.com>
2025-09-18 15:55:30 -0600

    EXAMEN 1.2

--- Figuras.py
+++ Figuras.py
@@ -7,3 +7,14 @@ class Cuadrado:
 
     def perimetro(self):
         return 4 * self.lado
+    
+class Rectangulo:
+    def __init__(self, ancho: float, alto: float): 
+        self.ancho = ancho
+        self.alto = alto
+
+    def area(self):
+        return self.ancho * self.alto
+
+    def perimetro(self):
+        return 2 * (self.ancho + self.alto)
